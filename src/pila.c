ce7886393c550198b6c327269e784f016ba6f038
oseidiaz5@gmail.com <oseidiaz5@gmail.com>
2026-05-16 17:04:00 -0600

    Aqui esta el codigo de pila.c de PostFija

--- src/pila.c
+++ src/pila.c
@@ -1,4 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "../include/pila.h"
+
+Pila* crearPila()
+	{
+		Pila* p= (Pila*)malloc(sizeof(Pila));
+		if (p != NULL)
+		{
+			p->tope = NULL;
+		}
+		return p;
+	}
+
+void push(Pila* p), void *dato, size_t size)
+	{
+		if (p == NULL) return; 
+		NodoPila* nuevo = (NodoPila*)malloc(sizeof(NodoPila));
+		if (nuevo == NULL) return; 
+
+		//reservar mamoria dinamica para el dato generico
+		nuevo->dato = malloc(size), 
+		if(nuevo->dato != NULL)
+		{
+			memcpy(nuevo->dato, dato, size);
+		}
+	
+		nuevo->siguiente = p->topes; 
+		p->tope = nuevo;
+	}
+
+char pop(Pila* p)
+	{
+		if(p == NULL || pilaVacia(p)) return '\0';
+
+		NodoPila* aux = p->tope;
+		char valor = *(char*)(aux->dato); //extraer el byte / char apurado
+
+		p->tope = p->tope->siguiente;
+		free(aux->dato);
+		free(aux);
+		return valor;
+	}
+
+char peek(Pila* p)
+	{
+		if(p == NULL || pilaVacia(p) return '\0');
+		return *(char*)(p->tope->dato);
+	}
+
+int pilaVacia(Pila* p)
+	{
+		return (p == NULL || p->tope = NULL);
+	}
+
+void destruirPila(Pila* p)
+	{
+		if(p == NULL) return;
+		while(!pilaVacia(p))
+		{
+			pop(p);
+		}
+		free(p);
+	}
